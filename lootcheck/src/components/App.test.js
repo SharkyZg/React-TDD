091e4b9172b6266bccab09e94790624e4f062352
Marko Sarkanj <marko.sarkanj@gmail.com>
2018-02-11 17:56:29 +0100

    lecture 29

---
+++ lootcheck/src/components/App.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+describe('App', () => {
+  const app = shallow(<App />);
+
+  it('renders properly', () => {
+    expect(app).toMatchSnapshot();
+  });
+
+  it('contains a connected wallet component', () => {
+    expect(app.find('Connect(Wallet)').exists()).toBe(true);
+  });
+});
