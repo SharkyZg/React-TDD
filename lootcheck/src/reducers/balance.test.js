19b39772e117c0ede4a062c8567e79d7ea045f18
Marko Sarkanj <marko.sarkanj@gmail.com>
2018-02-25 21:18:43 +0100

    lecture 31

--- lootcheck/src/reducers/balance.test.js
+++ lootcheck/src/reducers/balance.test.js
@@ -7,4 +7,18 @@ describe('balanceReducer', () => {
 
     expect(balanceReducer(undefined, { type: constants.SET_BALANCE, balance })).toEqual(balance);
   });
+
+  it('deposits into the balance', () => {
+    const deposit = 10;
+    const initialState = 5;
+
+    expect(balanceReducer(initialState, { type: constants.DEPOSIT, deposit })).toEqual(deposit + initialState)
+  });
+
+  it('withdraws from the balance', () => {
+    const withdraw = 10;
+    const initialState = 20;
+
+    expect(balanceReducer(initialState, { type: constants.WITHDRAW, withdraw })).toEqual(initialState - withdraw)
+  });
 });
