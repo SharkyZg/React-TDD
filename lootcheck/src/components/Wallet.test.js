d796118c3b3fb7e4cff282c08415d1bacf64905d
Marko Sarkanj <marko.sarkanj@gmail.com>
2018-02-26 08:11:33 +0100

    lecutre 32

--- lootcheck/src/components/Wallet.test.js
+++ lootcheck/src/components/Wallet.test.js
@@ -7,7 +7,9 @@ import { Wallet } from './Wallet'
 configure({ adapter: new Adapter() });
 
 describe('Wallet', () => {
-  const props = { balance: 20 }
+  const mockDeposit = jest.fn();
+  const mockWithdraw = jest.fn();
+  const props = { balance: 20, deposit: mockDeposit, withdraw: mockWithdraw }
   const wallet = shallow(<Wallet {...props} />);
 
   it('renders properly', () => {
@@ -32,5 +34,22 @@ describe('Wallet', () => {
     it('updates the local balance in ´state´ and converts it to a number', () => {
       expect(wallet.state().balance).toEqual(parseInt(userBalance, 10));
     });
+
+    describe('and the user wants to make a deposit', () =>{
+      beforeEach(() => wallet.find('.btn-deposit').simulate('click'));
+
+      it('dispatches the `deposit()` it receives from props with local balance', ()=>{
+        expect(mockDeposit).toHaveBeenCalledWith(parseInt(userBalance, 10));
+      });
+    });
+
+    describe('and the user wants to make a withdraw', () =>{
+      beforeEach(() => wallet.find('.btn-withdraw').simulate('click'));
+
+      it('dispatches the `withdraw()` it receives from props with local balance', ()=>{
+        expect(mockWithdraw).toHaveBeenCalledWith(parseInt(userBalance, 10));
+      });
+    });
+
   });
 });
